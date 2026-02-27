48bd89b25d1dd51786acbce10546004df076d691
Eric Swenson <eric.li.swenson@gmail.com>
2026-02-27 17:01:05 +0000

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # dotfiles
+
+Shell setup utilities.
+
+## Installation
+
+Run:
+
+`./install`
+
+## Notes
+
+- Dotfiles are symlinked into `$HOME` via GNU Stow.
+- Stow packages currently come from `agents/`.
---
+++ stow.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+DIR=$(cd "$(dirname "$0")" && pwd)
+
+COMMON_DIRS=(agents git-common)
+# DIRS that require symlinks when in a local env
+LOCAL_DIRS=()
+# DIRS that require symlinks when in a remote execution environment
+EXTERNAL_DIRS=()
+
+function run_stow(){
+  for dir in "$@"; do
+    if [ -d "$DIR/$dir" ]; then
+      stow -d "$DIR" -t "$HOME" "$dir"
+    else
+      echo "Skipping missing stow package: $dir"
+    fi
+  done
+}
+
+run_stow "${COMMON_DIRS[@]}"
+
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+  run_stow "${LOCAL_DIRS[@]}"
+fi
+
+if [[ $OSTYPE == 'linux-gnu'* ]]; then
+  run_stow "${EXTERNAL_DIRS[@]}"
+fi
